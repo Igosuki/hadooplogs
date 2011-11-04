ff8132fd411fe7b35ce52b781f9aa2b50d98c333
Guillaume Balaine <igosuki@gmail.com>
2011-11-04 16:01:23 +0100

    Further HQL testing with creation of external table and use of SerDe

--- src/test/java/load/jdbc/HiveJdbcClientTest.java
+++ src/test/java/load/jdbc/HiveJdbcClientTest.java
@@ -31,17 +31,12 @@ public class HiveJdbcClientTest {
 		//drop table if it exists
 		stmt.executeQuery("drop table " + tableName);
 		//create table with key, value
-		ResultSet res = stmt.executeQuery("create table " + tableName 
+		ResultSet res = stmt.executeQuery("create table if not exists " + tableName 
 				+ " (key int, value string, value2 string, value3 string, value4 string, value5 string)");
 		// show tables
 		System.out.println(res);
 		String sql = "show tables '" + tableName + "'";
 		printrun(sql);
-		res = execsql(stmt, sql);
-		Assert.assertTrue(res.getString(1).length() > 0);
-		if (res.next()) {
-			System.out.println(res.getString(1));
-		}
 		// describe table
 		sql = "describe " + tableName;
 		printrun(sql);
@@ -51,24 +46,35 @@ public class HiveJdbcClientTest {
 		}
 		Assert.assertTrue(res.getString(1).length() > 0);
 		Assert.assertTrue(res.getString(2).length() > 0);
-		// load data into table
-		// NOTE: filepath has to be local to the hive server
-		// NOTE: /tmp/a.txt is a ctrl-A separated file with two fields per line
-		String filepath = "./src/main/resources/testhivedriver.txt";
-		sql = "load data inpath '" + filepath + "' into table "
-				+ tableName;
-		printrun(sql);
-		res = execsql(stmt, sql);
+		StringBuilder query = new StringBuilder();
+		query.append("CREATE EXTERNAL TABLE IF NOT EXISTS my_table (");
+		query.append("field1 string, field2 int, field3 string, field4 double");
+		query.append(")");
+		query.append("ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.JsonSerde'");
+		query.append("LOCATION '/home/sesame/src/hivetests/'");
+		stmt.executeQuery(query.toString());
+		res = execsql(stmt, "select * from my_table limit 10");
+//		// load data into table
+//		// NOTE: filepath has to be local to the hive server
+//		// NOTE: /tmp/a.txt is a ctrl-A separated file with two fields per line
+//		String filepath = "./src/main/resources/testhivedriver.txt";
+//		sql = "load data inpath '" + filepath + "' into table "
+//				+ tableName;
+//		printrun(sql);
+//		res = execsql(stmt, sql);
 
 		// select * query
-		sql = "select * from " + tableName;
+		sql = "select * from my_table" ;
 		printrun(sql);
 		res = execsql(stmt, sql);
 		while (res.next()) {
-			System.out.println(String.valueOf(res.getInt(1)) + "\t"
-					+ res.getString(2));
+			while(res.next()) {
+				for (int i = 1; i <= 4; i++) {
+					System.out.print(String.valueOf(res.getString(i)) + "\t");
+				}
+				System.out.print("\n");
+			}
 		}
-
 		// regular hive query
 		sql = "select count(1) from " + tableName;
 		printrun(sql);
@@ -76,6 +82,15 @@ public class HiveJdbcClientTest {
 		while (res.next()) {
 			System.out.println(res.getString(1));
 		}
+		
+		StringBuilder queryEpo = new StringBuilder();
+		queryEpo.append("CREATE EXTERNAL TABLE IF NOT EXISTS my_table_epo (");
+		queryEpo.append("field1 string, field2 int, field3 string, field4 double");
+		queryEpo.append(")");
+		queryEpo.append("ROW FORMAT SERDE 'load.serde.filters.EpoSerDeFilter'");
+		queryEpo.append("LOCATION '/home/sesame/src/hivetests/'");
+		stmt.executeQuery(queryEpo.toString());
+		res = execsql(stmt, "select * from my_table_epo limit 10");
 	}
 
 	private static ResultSet execsql(Statement stmt, String sql)
--- src/test/java/load/serde/SerdeMarshallerTest.java
+++ src/test/java/load/serde/SerdeMarshallerTest.java
@@ -1,5 +1,17 @@
 package load.serde;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.apache.hadoop.hive.metastore.api.NoSuchObjectException;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.hive.service.HiveClient;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransportException;
 import org.junit.Test;
 
 public class SerdeMarshallerTest {
@@ -7,6 +19,49 @@ public class SerdeMarshallerTest {
 	
 	@Test
 	public void testMarshall() {
-		
+		TSocket transport = null;
+		try {
+			transport = new TSocket("sep347", 10000);
+			TBinaryProtocol protocol = new TBinaryProtocol(transport);
+			HiveClient client = new HiveClient(protocol);
+			transport.open();
+			System.out.println(String.valueOf(client.aliveSince()));
+			client.send_execute("drop table testtablejavaclient");
+			closeThenOpen(transport);
+			client.send_execute("create table testtablejavaclient (key int, value string)");
+			closeThenOpen(transport);
+			List<String> get_all_tables = client.get_all_tables("default");
+			for (String string : get_all_tables) {
+				System.out.print(string.concat(","));
+			}
+			closeThenOpen(transport);
+			StringBuilder query = new StringBuilder();
+			query.append("CREATE EXTERNAL TABLE IF NOT EXISTS my_table (");
+			query.append("field1 string, field2 int, field3 string, field4 double");
+			query.append(")");
+			query.append("ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.JsonSerde'");
+			query.append("LOCATION '/home/sesame/src/hivetests/'");
+			client.send_execute(query.toString());
+			closeThenOpen(transport);
+			Table table = client.get_table("default", "my_table");
+			table.getDbName();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (TException e) {
+			e.printStackTrace();
+		} catch (MetaException e) {
+			e.printStackTrace();
+		} catch (NoSuchObjectException e) {
+			e.printStackTrace();
+		} finally {
+			if(transport != null) {
+				transport.close();
+			}
+		}
+	}
+
+	private void closeThenOpen(TSocket transport) throws TTransportException {
+		transport.close();
+		transport.open();
 	}
 }
