026c861e71925e6c3a52882544ba5587661f02f1
Guillaume Balaine <igosuki@gmail.com>
2011-11-07 18:49:17 +0100

    basic script to use with CsvSerDe for 'in' conference files

---
+++ src/main/hql/serde/eposerdein.sql
@@ -0,0 +1,12 @@
+ADD JAR /home/sesame/src/hivejars/eposerde.jar;
+DROP TABLE IF EXISTS epo_normalized_table;
+CREATE EXTERNAL TABLE IF NOT EXISTS epo_normalized_table (field1 string, field2 string, field3 string)
+ROW FORMAT SERDE 'load.serde.filters.CsvSerDe'  
+LOCATION '/home/sesame/src/hivetests/epo/in'
+TBLPROPERTIES (
+'columns.types'='string,string,string',
+'columns.names'='publisher_docid,batchid_in,time_stamp',
+'columns.addsep'='_'
+)
+;
+select * from epo_normalized_table limit 10;
