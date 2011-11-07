0f68eefa39a94ea02b665ed7fda62527e6ad18a4
Guillaume Balaine <igosuki@gmail.com>
2011-11-07 11:19:29 +0100

    Constants classes. Included : Table properties relating to types and columns

---
+++ src/main/java/load/utils/Constants.java
@@ -0,0 +1,22 @@
+package load.utils;
+
+public final class Constants {
+
+	private Constants () {}
+	
+	public final class TableProps {
+		
+		// Table meta data constants
+		public static final String COLUMNS = "columns.names";
+		public static final String COLUMN_TYPES = "columns.types";
+		public static final String ADD_SEP_CHAR = "columns.addsep";
+		
+		// Type names constants
+		public static final String DOUBLE = "double";
+		public static final String INTEGER = "integer";
+		public static final String STRING = "string";
+		public static final String TIMESTAMP = "timestamp";
+		public static final String BOOLEAN = "boolean";
+		public static final String FLOAT = "float";
+	}
+}
