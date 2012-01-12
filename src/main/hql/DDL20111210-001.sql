2f8b05b2c06946ff101dbdf1aa6809975f070252
Nicola Lovadina <nicola@lovadina.org>
2012-01-12 19:00:59 +0100

    SOme minor changes

---
+++ src/main/hql/DDL20111210-001.sql
@@ -0,0 +1,31 @@
+-- This script creates the base table for the npl analisys
+ADD JAR /home/sesame/src/hivejars/eposerde.jar;
+CREATE TABLE  npl_normalized_log (delivery_id string, provider string, flowtype string, publisher_doc_id string, batch_id string,
+status string, tstamp string, delivery string , year string, month string, XP string)
+COMMENT 'This is a table for logs of DFP processes'
+ROW FORMAT SERDE 'load.serde.filters.CsvSerDe'
+STORED AS INPUTFORMAT 'load.hadoop.mapred.CsvInputFormat' OUTPUTFORMAT 'load.hadoop.mapred.CsvOutputFormat'
+LOCATION '/var/hadoop/data/epoin'
+TBLPROPERTIES (
+'columns.types'='string,string,string,string,string,string,string,string,string,string,string',
+'columns.names'='delivery_id,provider,flowtype,publisher_doc_id,batch_id,status,tstamp,delivery,year,month,XP'
+)
+
+CREATE TABLE npl_stat(delivery_id string, provider string, flowtype string, delivery string, status string, status_count int, min_xp string, max_xp string);
+
+CREATE TABLE npl_files_out(delivery_id string, bbl_file string, txt_file string)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\;'
+STORED AS TEXTFILE;
+
+CREATE TABLE npl_dates(delivery_id string, elaboration_date string, loader_date string)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\;'
+STORED AS TEXTFILE;
+
+CREATE TABLE npl_stat_condensed(DELIVERY_ID string, PROVIDER string, FLOWTYPE string, DELIVERY string, INN string, IGNORED string,
+BUSINNESSREJECT string, UNKNOWNERROR string, NOPDF string, UNKNOWNCONTENTTYPE string,OK string, PRODUCED string, FIRST_XP string, LAST_XP string,
+BBL_FILE string, TXT_FILE string, ELABORATION_DATE string, RUN_DATE string);
+
+
+
